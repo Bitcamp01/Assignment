dfc52c5c898bbc942152d476692a51c21f97793a
bitcamp\BIT <0215bitcamp@gmail.com>
2024-09-02 09:29:55 +0900

    2024-09-02 알고리즘 숙제

---
+++ algorithm/_1427.java
@@ -0,0 +1,55 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class _1427 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char[] arr=sc.nextLine().toCharArray();
+        quickSort(arr,0,arr.length-1);
+        for (char c : arr) {
+            System.out.print(c);
+        }
+    }
+
+    public static void quickSort(char[] arr, int left, int right) {
+        if (arr.length<=1){
+            return;
+        }
+        if (left < right) {
+            // 분할의 기준이 되는 피벗을 선택
+            int pivotIndex = partition(arr, left, right);
+
+            // 피벗을 기준으로 왼쪽 부분과 오른쪽 부분을 각각 정렬
+            quickSort(arr, left, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, right);
+        }
+    }
+
+    // 배열을 분할하는 메소드
+    private static int partition(char[] arr, int left, int right) {
+        char pivot = arr[right];  // 피벗을 배열의 마지막 요소로 설정
+        int i = left - 1;  // i는 작은 요소들의 마지막 인덱스를 가리킴
+
+        for (int j = left; j < right; j++) {
+            if (arr[j] > pivot) {
+                i++;
+                // arr[i]와 arr[j]의 위치를 교환
+                swap(arr, i, j);
+            }
+        }
+
+        // 피벗을 자신의 위치로 이동 (i+1 인덱스와 피벗 위치 교환)
+        swap(arr, i + 1, right);
+
+        // 새로운 피벗 위치 반환
+        return i + 1;
+    }
+
+    // 배열의 두 요소의 위치를 교환하는 메소드
+    private static void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}

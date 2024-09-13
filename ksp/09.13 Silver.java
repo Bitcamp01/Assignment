6f303101f4e430630b479bb98f5610e022584f5f
bit\bitcamp <1>
2024-09-13 17:01:30 +0900

    24.09.13/김시표 이분탐색

---
+++ ksp/09.13 Silver.java
@@ -0,0 +1,62 @@
+//N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+//
+//첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다. 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다. 모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다.
+//
+//M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(arr[mid] == target) {
+                return true;
+            } else if(arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        int[] B = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if(binarySearch(A, num)) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
